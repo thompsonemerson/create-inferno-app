fb9c8561942a79e89a05c14c8cc1cc22b0a50875
Dominic Gannaway <dg@domgan.com>
2016-12-13 23:07:01 +0000

    updated version to beta33

--- packages/inferno-scripts/scripts/init.js
+++ packages/inferno-scripts/scripts/init.js
@@ -59,13 +59,13 @@ module.exports = function(appPath, appName, verbose, originalDirectory) {
   });
 
   // Run another npm install for inferno and inferno-component
-  console.log('Installing inferno@beta32 and inferno-component@beta32 from npm...');
+  console.log('Installing inferno@beta33 and inferno-component@beta33 from npm...');
   console.log();
   // TODO: having to do two npm installs is bad, can we avoid it?
   var args = [
     'install',
-    'inferno@beta32',
-    'inferno-component@beta32',
+    'inferno@beta33',
+    'inferno-component@beta33',
     '--save',
     verbose && '--verbose'
   ].filter(function(e) { return e; });
