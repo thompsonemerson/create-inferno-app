feb792812ecf02c8e378f75a29564d6e1587e7c0
Sampo Kivistö <sampo.kivisto@live.fi>
2018-10-14 00:59:57 +0300

    replaced react with inferno in cra test

--- tasks/cra.js
+++ tasks/cra.js
@@ -96,14 +96,14 @@ Object.keys(packagePathsByName).forEach(name => {
 console.log('Replaced all local dependencies for testing.');
 console.log('Do not edit any package.json while this task is running.');
 
-// Finally, pack react-scripts.
+// Finally, pack inferno-scripts.
 // Don't redirect stdio as we want to capture the output that will be returned
 // from execSync(). In this case it will be the .tgz filename.
 const scriptsFileName = cp
-  .execSync(`npm pack`, { cwd: path.join(packagesDir, 'react-scripts') })
+  .execSync(`npm pack`, { cwd: path.join(packagesDir, 'inferno-scripts') })
   .toString()
   .trim();
-const scriptsPath = path.join(packagesDir, 'react-scripts', scriptsFileName);
+const scriptsPath = path.join(packagesDir, 'inferno-scripts', scriptsFileName);
 
 // Now that we have packed them, call the global CLI.
 cp.execSync('yarn cache clean');
@@ -111,7 +111,7 @@ cp.execSync('yarn cache clean');
 const args = process.argv.slice(2);
 
 // Now run the CRA command
-const craScriptPath = path.join(packagesDir, 'create-react-app', 'index.js');
+const craScriptPath = path.join(packagesDir, 'create-inferno-app', 'index.js');
 cp.execSync(
   `node ${craScriptPath} --scripts-version="${scriptsPath}" ${args.join(' ')}`,
   {
