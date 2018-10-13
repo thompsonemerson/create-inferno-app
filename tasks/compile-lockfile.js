6b5b916e53254f2d879de78a10b1c08f1eebf1c4
Sampo Kivistö <sampo.kivisto@live.fi>
2018-10-13 22:47:22 +0300

    Updated compile lockfile script

--- tasks/compile-lockfile.js
+++ tasks/compile-lockfile.js
@@ -23,24 +23,24 @@ try {
   // Create an empty package.json that we'll populate
   fse.writeFileSync(path.join(temp, 'package.json'), '{}');
 
-  // Extract the dependencies from react-scripts (which is a workspace)
-  const dependencies = require('react-scripts/package.json').dependencies;
+  // Extract the dependencies from inferno-scripts (which is a workspace)
+  const dependencies = require('inferno-scripts/package.json').dependencies;
   const descriptors = Object.keys(dependencies).map(
     dep => `${dep}@${dependencies[dep]}`
   );
 
-  // Run "yarn add" with all the dependencies of react-scripts
+  // Run "yarn add" with all the dependencies of inferno-scripts
   cprocess.execFileSync('yarn', ['add', ...descriptors], { cwd: temp });
 
-  // Store the generated lockfile in create-react-app
-  // We can't store it inside react-scripts, because we need it even before react-scripts is installed
+  // Store the generated lockfile in create-inferno-app
+  // We can't store it inside inferno-scripts, because we need it even before inferno-scripts is installed
   fse.copySync(
     path.join(temp, 'yarn.lock'),
     path.join(
       __dirname,
       '..',
       'packages',
-      'create-react-app',
+      'create-inferno-app',
       'yarn.lock.cached'
     )
   );
